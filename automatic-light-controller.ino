ef4d6e6cf54815b6d21de85df676ba99017cd86a
Artem Pronchakov <artem.pronchakov@calisto.email>
2015-10-03 19:29:49 +0300

    Change time values for pruduction

--- automatic-light-controller.ino
+++ automatic-light-controller.ino
@@ -2,13 +2,13 @@ int sensorPin = A0;
 int relayPin = 13;
 
 int sensorValue = 0;
-int second = 1000;
-int delayValue = second;
-int loopsBeforeInitialState = 10;
+int delayValue = 1000;
+int loopsBeforeInitialState = 180;
 int loopsCurrent = 0;
 
 void setup() {
   pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
   Serial.begin(9600);
 }
 
